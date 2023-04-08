8be60908fc23f0abe065a9a4b3edbf4b7941d525
olabobo <olaitanopeyemi21@gmail.com>
2023-04-08 19:04:59 +0100

    now

--- src/App.js
+++ src/App.js
@@ -4,21 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <>
+        <header className="head">
+            <h1> Welcome to my website</h1>
+        
+        <nav> 
+        <ul>
+         <li><a href="#">Home</a></li>
+         <li><a href="#">About</a></li>
+         <li><a href="#">Services</a></li>
+         <li><a href="#">Contact</a></li>
+       </ul>
+        </nav>
+        </header>
+        </>
+    
+
+<main>
+<section>
+  <h2>About Us</h2>
+  <p>
+    We are a small team of web developers based 
+    in the United States. We
+    specialise in creating modern and responsive 
+    websites for businesses  and individuals.
+  </p>
+</section>
+<section>
+  <h2>Our Services</h2>
+  <ul>
+    <li>Web Design</li>
+    <li>Web Development</li>
+    <li>E-commerce Solutions</li>
+    <li>Search Engine Optimization (SEO)</li>
+  </ul>
+</section>
+</main>
+</div>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+function  header() {
+    return(
+        <>
+        <header className="head">
+            <h1> Welcome to my website</h1>
+        
+        <nav> 
+        <ul>
+         <li><a href="#">Home</a></li>
+         <li><a href="#">About</a></li>
+         <li><a href="#">Services</a></li>
+         <li><a href="#">Contact</a></li>
+       </ul>
+        </nav>
+        </header>
+        </>
+    );
+}
